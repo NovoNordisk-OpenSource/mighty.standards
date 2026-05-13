31e808e9485f38f54835c49fd81f4e7b30784629
akselthomsen <thomsenaksel@gmail.com>
2026-05-13 15:31:09 +0200

    feat: add CI workflow and dummy component

---
+++ components/dummy/test-dummy.R
@@ -0,0 +1,12 @@
+test_that("dummy", {
+  dummy <- mighty.component::get_test_component(
+    component = file.path("components", "dummy", "dummy.mustache"),
+    params = list(domain = "df", variable = "y", value = 1)
+  )
+
+  df <- data.frame(x = "a", y = 1)
+
+  dummy$assign(x = "df", value = dplyr::select(df, -y))
+  dummy$eval()$get("df") |>
+    expect_equal(df)
+})
